b386ba3e90e221440a99884b85e502a59beaea94
Ryan Whittingham <whittingham777@gmail.com>
2020-01-19 19:43:09 -0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DecisionMakerApp from './components/DecisionMakerApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<DecisionMakerApp />, document.getElementById('app'));
